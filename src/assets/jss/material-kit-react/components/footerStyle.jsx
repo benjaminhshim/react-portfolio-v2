1e57f1025524fe84e7de2f91cdf9c0b7bab45859
Benjamin Shim <benjaminshim@Benjamins-MacBook-Pro.local>
2018-08-07 16:40:04 -0700

    Fix footer

--- src/assets/jss/material-kit-react/components/footerStyle.jsx
+++ src/assets/jss/material-kit-react/components/footerStyle.jsx
@@ -18,8 +18,8 @@ const footerStyle = {
   },
   right: {
     padding: "15px 0",
-    margin: "0",
-    float: "right"
+    float: "right",
+    margin: "0"
   },
   footer: {
     padding: "0.9375rem 0",
